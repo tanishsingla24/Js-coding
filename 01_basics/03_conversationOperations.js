b1900bc0fb4bf85d7011b809908dc89e74f14fdd
Tanish singla <161108766+tanishsingla24@users.noreply.github.com>
2025-04-04 06:57:13 +0000

    converstion of datatypes

---
+++ 01_basics/03_conversationOperations.js
@@ -0,0 +1,27 @@
+// let score = "33a"
+// score = null
+// score = undefined
+// score = true
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33"=> 33
+// "33abs" => not a number NaN
+// true => 1
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 0 => false
+// "" => false
+// "string" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
